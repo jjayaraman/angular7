59fac76ae561b37ebd841dd62ac697af1d41923c
Jayakumar Jayaraman <india.jai@gmail.com>
2018-11-11 19:39:24 +0000

    updated

--- reactive-forms/src/app/app-routing.module.ts
+++ reactive-forms/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'simple', component: SimpleFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- reactive-forms/src/app/app.module.ts
+++ reactive-forms/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
