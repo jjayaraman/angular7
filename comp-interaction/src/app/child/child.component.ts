1c2b6bbf003a140e0abb9eac94408fe34272683f
Jayakumar Jayaraman <india.jai@gmail.com>
2019-01-15 07:50:54 +0000

    @Output added

--- comp-interaction/src/app/child/child.component.ts
+++ comp-interaction/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,12 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
-
-  @Input() fromParentMsg:string;
-  fromChildMsg:string = 'default child message';
   count:number = 1;
   constructor() { }
 
+  // Reads parent message
+  @Input() fromParentMsg:string;
+
+  // Sends child message to parent using @ViewChild()
+  fromChildMsg:string = 'default child message';
+
+  // Sends child message to parent using @Output()
+  @Output() childMessageEvent = new EventEmitter<String>();
 
   ngOnInit() {
     console.log('child oninit...', this.fromParentMsg);
@@ -25,4 +30,9 @@ export class ChildComponent implements OnInit {
   childClicked() {
     this.fromChildMsg = '@ViewChild() from child ' ,this.count++;
   }
+
+  sendChildMsgtoParent() {
+    this.childMessageEvent.emit('Message from child using @Output');
+  }
+
 }
--- comp-interaction/src/app/parent/parent.component.ts
+++ comp-interaction/src/app/parent/parent.component.ts
@@ -7,12 +7,12 @@ import { ChildComponent } from '../child/child.component';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit, AfterViewInit {
-
   fromParentMsg = 'defaul parent msg';
   count = 1;
+  count2 = 1 ;
 
   @ViewChild(ChildComponent) child;
-
+  msg2;
   constructor() {}
 
   ngOnInit() {
@@ -22,11 +22,15 @@ export class ParentComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     console.log('parent after view init');
     console.log('childMessage : ', this.child.fromChildMsg);
-
-
   }
 
   parentClick() {
     this.fromParentMsg = 'parent updated its message...' + this.count++;
+  }
+
+
+  receiveMessageEvent(event) {
+    console.log(event);
+    this.msg2 = event + ' : ' + this.count2++;
   }
 }
