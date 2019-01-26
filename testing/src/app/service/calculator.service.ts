1f4ebdad832c2ede1e7c2930934ad3d99bea7209
Jayakumar Jayaraman <india.jai@gmail.com>
2019-01-26 18:18:34 +0000

    test added

---
+++ testing/src/app/service/calculator.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  constructor() { }
+
+  add(x: number, y: number): number  {
+    return x + y;
+  }
+}
