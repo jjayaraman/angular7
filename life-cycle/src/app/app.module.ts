106b7211dd500d26b8372aa2756e62e1e038261f
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2019-03-13 17:38:02 +0000

    added

---
+++ life-cycle/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OneComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
